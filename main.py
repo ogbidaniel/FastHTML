66369e3bdbed772c551193cf510528b9396a86b9
Daniel Ogbuigwe <103327095+ogbidaniel@users.noreply.github.com>
2024-10-22 22:29:21 +0100

    FastHTML

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 # main.py
-from fasthtml import common as fh
+from fasthtml.common import *
 
-app, rt = fh.fast_app(live=True)
+app, rt = fast_app(live=True)
+
+contacts = [
+    {"name": "Jane Doe", "email": "john.doe@example.com"},
+    {"name": "John Doe", "email": "john.doe@example.com"},
+    {"name": "Bob Smith", "email": "bob.smith@example.com"}
+]
+
+items = Ul(*[Li(o["name"]) for o in contacts])
 
 @rt('/')
-def get(): return fh.Div(fh.P('Live FastHTML Application!'))
-
-fh.serve()
+def get():
+    return Titled("Contact Book", Div(
+        Ul(*items),
+        H2('Add a new contact'),
+        Button("Click me", onclick="alert('Hello World')")
+    )
+    )
+    
+serve()
