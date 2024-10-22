c0c6cd5d53d3ed2fbcd25f55dc9e17de02951fd2
Daniel Ogbuigwe <103327095+ogbidaniel@users.noreply.github.com>
2024-10-23 00:01:25 +0100

    finalise tutorial code

--- main.py
+++ main.py
@@ -1,23 +1,62 @@
 # main.py
 from fasthtml.common import *
 
-app, rt = fast_app(live=True)
+def render(contact):
+    return Li(f"{contact.name} - {contact.email}",
+              A("Delete", hx_delete=f"/{contact.id}", hx_swap="outerHTML",
+                target_id=f"contact-{contact.id}"),
+              id=f"contact-{contact.id}")
 
-contacts = [
-    {"name": "Jane Doe", "email": "john.doe@example.com"},
-    {"name": "John Doe", "email": "john.doe@example.com"},
-    {"name": "Bob Smith", "email": "bob.smith@example.com"}
-]
+app, rt, contacts, Contact = fast_app(
+    'contacts.db',
+    live=True,
+    render=render,
+    id=int,
+    name=str,
+    email=str,
+    pk='id'
+)
 
-items = Ul(*[Li(o["name"]) for o in contacts])
+def create_contacts():
+    if len(contacts()) == 0:
+        contacts.insert(dict(name="Jane Doe", email="jane.doe@example.com"))
+        contacts.insert(dict(name="John Doe", email="john.doe@example.com"))
+        contacts.insert(dict(name="Bob Smith", email="bob.smith@example.com")
+    )
+
 
 @rt('/')
 def get():
-    return Titled("Contact Book", Div(
-        Ul(*items),
-        H2('Add a new contact'),
-        Button("Click me", onclick="alert('Hello World')")
-    )
-    )
     
+    create_contacts()
+    
+    return Titled("Contact Book", Div(
+        Ul(*contacts(), id="contacts_list"),
+        
+        H2('Add a new contact'),
+        Form(
+            reset_name_input(),
+            reset_email_input(),
+            Button("Add Contact", hx_post="/", target_id="contacts_list", hx_swap="beforeend"),
+        )
+    )
+    )
+
+def reset_name_input():
+    return Input(id="name", placeholder="Name", hx_swap_oob="true")
+
+def reset_email_input():
+    return Input(id="email", placeholder="Email", hx_swap_oob="true")
+
+@rt('/')
+def post(contact: Contact):
+    contacts.insert(contact)
+    return contact, reset_name_input(), reset_email_input()
+
+@rt('{/}')
+def delete(id:int):
+    contacts.delete(id)
+
+serve()
+
 serve()
